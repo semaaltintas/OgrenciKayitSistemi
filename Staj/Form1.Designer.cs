5451642955cd51cf17d85313e77fd903049ce4b8
Sema Altıntaş <altnts_sema@hotmail.com>
2022-06-04 15:20:19 +0300

    Proje dosyası ekle.

---
+++ Staj/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace Staj
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tc Kimlik No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Öğrenci No";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Öğrenci Adı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Öğrenci Soyadı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Öğrenci Sınıfı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(200, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(200, 128);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 2;
+            this.maskedTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.maskedTextBox1_KeyPress);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(200, 173);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 2;
+            this.maskedTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.maskedTextBox2_KeyPress);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(200, 222);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ara";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(181, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(312, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(448, 288);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Formu Temizle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(50, 288);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Kaydet";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 317);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(562, 181);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 510);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Staj/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//System.Data.oledb Kütüphanesinin tanımlanması
+using System.Data.OleDb;
+//Giriş-Çıkış işlemlerine ilişkin kütüphanenin tanımlanması
+using System.IO;
+namespace Staj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Veri tabanı dosya yolu ve provider nesnesinin belirlenmesi
+        OleDbConnection baglantim = new OleDbConnection("Provider=Microsoft.Ace.OleDb.12.0;Data Source=Ogrenci.accdb");
+        public static string Ogr_Tc_No, Ogr_No, Ogr_Adi, Ogr_Soyadi, Ogr_Sinif;
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length < 6)
+                errorProvider1.SetError(textBox2, "Öğrenci No 6 karakter olmalı!");
+            else
+                errorProvider1.Clear();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57) || (int)e.KeyChar == 8)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57) || (int)e.KeyChar == 8)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void maskedTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) == true || char.IsControl(e.KeyChar) == true || char.IsSeparator(e.KeyChar) == true)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void maskedTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsLetter(e.KeyChar) == true || char.IsControl(e.KeyChar) == true || char.IsSeparator(e.KeyChar) == true)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length < 11)
+                errorProvider1.SetError(textBox1, "Tc Kimlik No 11 karakter olmalı!");
+            else
+                errorProvider1.Clear();
+        }
+        private void ogrecileri_goster()
+        {
+            try
+            {
+                baglantim.Open();
+                OleDbDataAdapter ogrencileri_listele = new OleDbDataAdapter("select Ogr_Tc_No AS[Öğrenci Tc Kimlik No]," +
+                    "Ogr_No AS[Öğrenci Numarası],Ogr_Adi AS[Öğrenci Adı],Ogr_Soyadi AS[Öğrenci Soyadı],Ogr_Sinifi AS[Öğrenci Sınıfı] " +
+                    "from ogrenciler Order By Ogr_Adi ASC", baglantim);
+                DataSet dshafiza = new DataSet();
+                ogrencileri_listele.Fill(dshafiza);
+                dataGridView1.DataSource = dshafiza.Tables[0];
+                baglantim.Close();
+            }
+            catch (Exception hatamsj)
+            {
+                MessageBox.Show(hatamsj.Message, "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                baglantim.Close();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ogrecileri_goster();
+            maskedTextBox1.Mask = "LL???????????????????????";
+            maskedTextBox2.Mask = "LL???????????????????????";
+            textBox2.MaxLength = 6;
+            textBox1.MaxLength = 11;
+            toolTip1.SetToolTip(this.textBox1, "Tc Kimlik No 11 Karaketer Olmalı!");
+            toolTip2.SetToolTip(this.textBox2, "Öğrenci No 6 Karakter olmalı!");
+            maskedTextBox1.Text.ToUpper();
+            maskedTextBox2.Text.ToUpper();
+            comboBox1.Items.Add("1"); comboBox1.Items.Add("2"); comboBox1.Items.Add("3"); comboBox1.Items.Add("4");
+        }
+        private void topPage1_temizle()
+        {
+            textBox1.Clear();textBox2.Clear();maskedTextBox1.Clear();maskedTextBox2.Clear();comboBox1.SelectedIndex = -1;
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool kayit_arama_durumu = false;
+            if (textBox1.Text.Length == 11)
+            {
+                baglantim.Open();
+                OleDbCommand selectsorgu = new OleDbCommand("select * from ogrenciler where Ogr_Tc_No = '" + textBox1.Text + "'", baglantim);
+                OleDbDataReader kayitokuma = selectsorgu.ExecuteReader();
+                while (kayitokuma.Read())
+                {
+                    kayit_arama_durumu = true;
+                    textBox2.Text = kayitokuma.GetValue(1).ToString();
+                    maskedTextBox1.Text = kayitokuma.GetValue(2).ToString();
+                    maskedTextBox2.Text = kayitokuma.GetValue(3).ToString();
+                    comboBox1.Text = kayitokuma.GetValue(4).ToString();
+                    break;
+                }
+                if (kayit_arama_durumu == false)
+                {
+                    MessageBox.Show("Aranan Kayıt Bulunamadı!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                baglantim.Close();
+            }
+            else
+            { MessageBox.Show("Lütfe 11 Haneli Tc Kimlik No Giriniz!","Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+              topPage1_temizle(); 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+                //Tc kimlik no kontrol
+                if (textBox1.Text.Length < 11 || textBox1.Text == "")
+                    label1.ForeColor = Color.Red;
+                else
+                    label1.ForeColor = Color.Black;
+                //Öğrenci no kontrol
+                if (textBox2.Text.Length < 6 || textBox2.Text == "")
+                    label2.ForeColor = Color.Red;
+                else
+                    label2.ForeColor = Color.Black;
+                //Öğrenci adı kontrol
+                if (maskedTextBox1.MaskCompleted == false)
+                    label3.ForeColor = Color.Red;
+                else
+                    label3.ForeColor = Color.Black;
+                //Öğrenci soyadı kontrol
+                if (maskedTextBox2.MaskCompleted == false)
+                    label4.ForeColor = Color.Red;
+                else
+                    label4.ForeColor = Color.Black;
+                //Öğrenci sınıf kontrolu
+                if (comboBox1.Text == "")
+                    label5.ForeColor = Color.Red;
+                else
+                    label5.ForeColor = Color.Black;
+
+                if (textBox1.Text.Length == 11 && textBox1.Text != "" && textBox2.Text != "" && textBox2.Text.Length == 6 && maskedTextBox1.MaskCompleted != false && maskedTextBox2.MaskCompleted != false && comboBox1.Text != "")
+                {
+                    try
+                    {
+                        baglantim.Open();
+                        OleDbCommand guncellekomutu = new OleDbCommand("update ogrenciler set Ogr_No = '" + textBox2.Text + "', Ogr_Adi = '" + maskedTextBox1.Text + "' , Ogr_Soyadi = '" + maskedTextBox2.Text + "' , Ogr_Sinifi = '" + comboBox1.Text + "' where Ogr_Tc_No = '"+textBox1.Text+"'" ,baglantim);
+                        guncellekomutu.ExecuteNonQuery();
+                        baglantim.Close();
+                        MessageBox.Show("Kayıt Güncellendi!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        ogrecileri_goster();
+                    }
+                    catch (Exception hatamsj)
+                    {
+                        MessageBox.Show(hatamsj.Message,"Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        baglantim.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Yazı rengi kırmızı olan alanları gözden geçiriniz!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool kayitkontrol = false;
+            baglantim.Open();
+            OleDbCommand selectsorgu = new OleDbCommand("select * from ogrenciler where Ogr_Tc_No='" + textBox1.Text + "'", baglantim);
+            OleDbDataReader kayitokuma = selectsorgu.ExecuteReader();
+            while (kayitokuma.Read())
+            {
+                kayitkontrol = true;
+                break;
+            }
+            baglantim.Close();
+            if (kayitkontrol == false)
+            {
+                //Tc kimlik no kontrol
+                if (textBox1.Text.Length < 11 || textBox1.Text == "")
+                    label1.ForeColor = Color.Red;
+                else
+                    label1.ForeColor = Color.Black;
+                //Öğrenci no kontrol
+                if (textBox2.Text.Length < 6 || textBox2.Text == "")
+                    label2.ForeColor = Color.Red;
+                else
+                    label2.ForeColor = Color.Black;
+                //Öğrenci adı kontrol
+                if (maskedTextBox1.MaskCompleted == false)
+                    label3.ForeColor = Color.Red;
+                else
+                    label3.ForeColor = Color.Black;
+                //Öğrenci soyadı kontrol
+                if (maskedTextBox2.MaskCompleted == false)
+                    label4.ForeColor = Color.Red;
+                else
+                    label4.ForeColor = Color.Black;
+                //Öğrenci sınıf kontrolu
+                if (comboBox1.Text == "")
+                    label5.ForeColor = Color.Red;
+                else
+                    label5.ForeColor = Color.Black;
+
+                if(textBox1.Text.Length == 11 && textBox1.Text != "" && textBox2.Text != "" && textBox2.Text.Length == 6 && maskedTextBox1.MaskCompleted != false && maskedTextBox2.MaskCompleted != false && comboBox1.Text != "")
+                {
+                    try
+                    {
+                        baglantim.Open();
+                        OleDbCommand eklekomutu = new OleDbCommand("insert into ogrenciler values ('" + textBox1.Text + "','" + textBox2.Text + "','" + maskedTextBox1.Text + "','" + maskedTextBox2.Text + "','" + comboBox1.Text + "')", baglantim);
+                        eklekomutu.ExecuteNonQuery();
+                        baglantim.Close();
+                        MessageBox.Show("Yeni Kayıt Oluşturuldu!","Öğrenci Kayıt Sistemi",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                        ogrecileri_goster();
+                        topPage1_temizle();
+                    }
+                    catch(Exception hatamsj)
+                    {
+                        MessageBox.Show(hatamsj.Message);
+                        baglantim.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Yazı rengi kırmızı olan alanları gözden geçiriniz!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Girilen Tc Kimlik Numarası önceden kayıtlıdır!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 11)
+            {
+                bool kayit_arama_durumu = false;
+                baglantim.Open();
+                OleDbCommand selectsorgu = new OleDbCommand("select * from ogrenciler where Ogr_Tc_No= '" + textBox1.Text + "'", baglantim);
+                OleDbDataReader kayitokuma = selectsorgu.ExecuteReader();
+                while (kayitokuma.Read())
+                {
+                    kayit_arama_durumu = true;
+                    OleDbCommand deletesorgu = new OleDbCommand("delete from ogrenciler where Ogr_Tc_No='" + textBox1.Text + "'", baglantim);
+                    deletesorgu.ExecuteNonQuery();
+                    MessageBox.Show("Öğrenci Kaydı Silindi", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    baglantim.Close();
+                    ogrecileri_goster();
+                    topPage1_temizle();
+                    break;
+                }
+                if (kayit_arama_durumu == false)
+                    MessageBox.Show("Silinecek Kayıt Bulunamadı!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                baglantim.Close();
+                topPage1_temizle();
+            }
+            else
+                MessageBox.Show("Lütfen 11 Haneli Tc Kimlik No Giriniz!", "Öğrenci Kayıt Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            topPage1_temizle();
+        }
+    }
+}
